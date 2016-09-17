ecee5ebc34dde73075ee882ada568c48e60c0e45
kashgarboys <2251220502@qq.com>
2016-09-17 20:02:01 +0800

    Finish Homework 2

--- main.ts
+++ main.ts
@@ -1,1 +1,19 @@
 console.log("my name is Ali")
+function output(line:number){
+for (var i=1;i<=line;i++){
+    var k =""
+
+    for (var s=1;s<=line - i;s++){
+        k+=" "
+    }
+
+    for(var j=1;j<=2*i-1;j++){
+
+        k+="*"
+           
+    }
+    console.log(k) 
+
+}}
+
+output(5)
